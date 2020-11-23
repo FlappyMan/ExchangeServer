c46c82acaf45a5321c7fef27212c923e8e179687
gong <lannister@ukex.com>
2020-11-22 21:33:18 -0800

    add test file

--- Trade/TradeLogMove/main.cpp
+++ Trade/TradeLogMove/main.cpp
@@ -33,6 +33,9 @@ bool Init(int argc, char *argv[])
 
     return true;
 }
+void Add(const int &a,const int &b){
+    std::cout<<a+b<<std::endl;
+}
 
 
 int main(int argc, char *argv[])
@@ -42,6 +45,132 @@ int main(int argc, char *argv[])
         cout<<"Init failed"<<endl;
         return -1;
     }
+#if 0
+    // shared_ptr<string> films[5] ={
+	// shared_ptr<string> (new string("Fowl Balls")),
+	// shared_ptr<string> (new string("Duck Walks")),
+	// shared_ptr<string> (new string("Chicken Runs")),
+	// shared_ptr<string> (new string("Turkey Errors")),
+	// shared_ptr<string> (new string("Goose Eggs"))
+	// };
+    // shared_ptr<string> pwin;
+    // pwin = films[2]; // films[2] loses ownership. 将所有权从films[2]转让给pwin，此时films[2]不再引用该字符串从而变成空指针
+
+	// cout << "The nominees for best avian baseballl film are\n";
+	// for(int i = 0; i < 5; ++i)
+	// {
+	// 	cout << *films[i] << endl;
+	// }
+	// int N, n;
+	// while (cin >> N) 
+    // {
+    //     int a[1001] = { 0 };
+	//     while (N--) 
+    //     {
+	// 	    cin >> n;
+	// 	    a[n] = 1;
+    //     }
+	//     for (int i = 0; i < 1001; i++)
+	//         if (a[i])
+	// 		    cout << i << endl;
+	// }
+    // string str1,str2;
+    // getline(cin,str1);
+    // getline(cin,str2);
+    // vector<string> vec;
+    // int len1=str1.length();
+    // int len2=str2.length();
+    // int m = len1/8;
+    // int n = len2/8;
+    // if(m>0)
+    // {    
+    //     for(int i=0;i<m;++i)
+    //     {
+    //         vec.push_back(str1.substr(i*8,8));
+    //     }
+    //     vec.push_back(str1.substr(m*8,8));
+    // }else{
+    //     vec.push_back(str1);
+    // }
+    // if(n>0)
+    // {
+    //      for(int i=0;i<n;++i)
+    //      {
+    //          vec.push_back(str2.substr(i*8,8));
+    //      }
+    //      vec.push_back(str2.substr(n*8,8)); 
+    // }else{
+    //     vec.push_back(str2);
+    // }
+    // for(int i=0;i<vec.size();++i)
+    // {
+    //     cout<<vec[i]<<endl;
+    // }
+
+
+    // int S,N;
+    // while(cin>>S>>N)
+    // {
+    //     int sum=0,tmp=-1;
+    //     for(int i=1;i<N;++i)
+    //         sum+=i;
+    //     int tot=S-sum;
+    //     if(tot%N!=0)
+    //         cout<<tmp<<endl;
+    //     for(int i=0;i<N;++i)
+    //     {
+    //         cout<<tot/N+i<<' ';
+    //     }        
+    // }
+
+
+    // int num;
+    // while(cin>>num)
+    // {
+    //     begin:
+    //     for(int i=2;i<num;++i)
+    //         for(int j=i+1;j<num;++j){
+    //         if(num%i==0 && num%j==0 && i*j==num){
+    //             for(int k=2;k<i;++k){
+    //                 if(i%k==0)
+    //                     goto begin;
+    //             }
+    //             for(int k=2;k<j;++k){
+    //                 if(j%k==0)
+    //                     goto begin;
+    //             }
+    //             cout<<i<<' '<<j<<endl;
+    //         } 
+    //         if(i==num||j==num)
+    //         {
+    //            cout<<-1<<-1<<endl;    
+    //         }         
+    //     }
+
+    // }
+
+    int num;
+    while(cin>>num)
+    {
+begin:
+        for(int i=2;i<num;++i)
+            for(int j=i;j<num;++j){
+            if(i*j==num){
+                for(int k=2;k<i;++k)
+                    if(i%k==0)
+                        goto begin;
+                for(int k=2;k<j;++k)
+                    if(j%k==0)
+                        goto begin;
+                cout<<i<<' '<<j<<endl;
+                break;
+            }
+            //cout<<-1<<-1<<endl;
+        }
+
+    }
+
+#else
     int64_t tBegin,tEnd;
     tBegin = GetCurTime();
     tp::TaskExecutionMgr<tp::TaskQueue<TableData>> *tskExecMgr = new tp::TaskExecutionMgr<tp::TaskQueue<TableData>>;
@@ -55,5 +184,6 @@ int main(int argc, char *argv[])
     tEnd = GetCurTime();
     cout<<"Elapsed time : "<<(tEnd-tBegin)<<endl;
     delete tskExecMgr;
+#endif
     return 0;
 }
